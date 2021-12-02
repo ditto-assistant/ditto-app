b9becfa301749d7d6c5a1853340a5ff3a5a7a7c9
Omar Barazanji <ozanji345@gmail.com>
2021-12-02 00:53:23 -0600

    model selector working

--- main.py
+++ main.py
@@ -13,47 +13,100 @@ class Assistant:
         self.speech = Speech()
         self.command = Command()
         self.prompt = ""
+        self.application = "model-selector"
+        self.activation_mode = True
 
     def get_speech(self):
         pass
 
+    def send_command(self):
+
+        if self.application == 'light-application': # light application logic
+            self.command.send_request(self.prompt, self.application)
+            self.speech.text = ""
+            self.speech.activation.text = ""
+            self.command_response = self.command.response.choices.copy().pop()['text'].split('\nA: ')[1]
+            if 'toggle-light' in self.command_response:
+                if 'off' in self.command_response:
+                    self.command.toggle_light('off')
+                    print('Turning off the lights')
+                    self.activation_mode = True # go back to idle...
+                    self.application = 'model-selector'
+                elif 'on' in self.command_response:
+                    self.command.toggle_light('on')
+                    print('Turning on the lights')
+                    self.activation_mode = True # go back to idle...
+                    self.application = 'model-selector'
+                elif 'sparkle' in self.command_response:
+                    self.command.toggle_light('sparkle')
+                    print('Setting lights to sparkle')
+                    self.activation_mode = True # go back to idle...
+                    self.application = 'model-selector'
+                else:
+                    print('invalid mode: %s' % self.command_response)
+                    self.activation_mode = True # go back to idle...
+                    self.application = 'model-selector'
+            else:
+                print('invalid command')
+
+        elif self.application == "conversation-application": # conversation application logic
+            self.command.send_request(self.prompt, self.application)
+            self.speech.text = ""
+            self.speech.activation.text = ""
+            self.command_response = self.command.response.choices.copy().pop()['text'].split('\nA: ')[1]
+            if 'toggle-conversation' in self.command_response:
+                reply = self.command_response.strip("toggle-conversation `").strip("`")
+                if '\\n' in reply:
+                    reply = reply.split('\\n')
+                    print('\nA: ')
+                    for x in reply:
+                        print(x)
+                else: print('A: '+ reply)
+                self.activation_mode = True # go back to idle...
+                self.application = 'model-selector'
+            else:
+                print('invalid reply')
+                self.activation_mode = True # go back to idle...
+                self.application = 'model-selector'
+
+
+        elif self.application == 'model-selector': # model selector logic
+            self.prompt = self.speech.text
+            self.command.send_request(self.prompt, self.application)
+            self.command_response = self.command.response.choices.copy().pop()['text'].split('\nA: ')[1]
+            if 'light-application' in self.command_response:
+                self.application = 'light-application'
+            elif 'conversation-application'in self.command_response:
+                self.application = 'conversation-application'
+            else:
+                self.activation_mode = True # go back to idle...
+                print('invalid application')
+
     def activation_sequence(self):
-        self.speech.record_audio(True)
+        self.activation_mode = True
+        self.speech.record_audio(activation_mode=True) # record audio and listen for name
         if self.speech.activation.activate: # name has been spoken
             self.speech.activation.activate = False
             self.speech.text = ""
             self.speech.activation.text = ""
 
+            # print("["+self.application+"]")
             print('   ,.,')
             print(' ((~"~))')
             print("'(|o_o|)'")
             print(",..\=/..,")
 
-            self.speech.record_audio(False)
-            if self.speech.activation.activate: 
+            self.activation_mode = False
+            self.speech.record_audio(activation_mode=False) # record audio and listen for command
+            if self.speech.activation.activate: # command has been spoken
                 print('sending request to GPT3')
                 self.speech.activation.activate = False
-                self.command.send_request(self.speech.text)
 
-                self.speech.text = ""
-                self.speech.activation.text = ""
+                ## enter application handler ## (main loop)
+                while not self.activation_mode:
+                    self.send_command()
 
-                self.command_response = self.command.response.choices.copy().pop()['text'].strip('.\nA: ')
-
-                if 'toggle-light' in self.command_response:
-                    if 'off' in self.command_response:
-                        self.command.toggle_light('off')
-                        print('Turning off the lights')
-                    elif 'on' in self.command_response:
-                        self.command.toggle_light('on')
-                        print('Turning on the lights')
-                    elif 'sparkle' in self.command_response:
-                        self.command.toggle_light('sparkle')
-                        print('Setting lights to sparkle')
-                    else:
-                        print('invalid mode: %s' % self.command_response)
-                else:
-                    print('invalid command')
+                
 
             else: print('Canceling...')
                 
