d2fbc672c33999d28bd4fd64fb751f14535568d8
Omar Barazanji <ozanji345@gmail.com>
2021-12-03 21:07:31 -0600

    added spotify web api integration and crated spotify-application

--- command.py
+++ command.py
@@ -16,12 +16,14 @@ import openai
 import serial
 
 from modules.hourglass.timer import Timer
+from modules.spotify.spotify import Spotify
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class Command:
 
-    def __init__(self):
+    def __init__(self, path):
+        self.path = path
         self.light_status = True
         self.light_mode = 'default'
         self.response = ''
@@ -29,7 +31,7 @@ class Command:
 
     def toggle_light(self, mode):
         try:
-            s =serial.Serial('COM3', baudrate=9600, bytesize=8)
+            s = serial.Serial('COM3', baudrate=9600, bytesize=8)
             if mode == 'on':
                 s.write(b'\x00')
                 self.light_status = True
@@ -46,9 +48,22 @@ class Command:
         except:
             print('no device found')
 
+
     def toggle_timer(self, val):
         timer = Timer()
         timer.set_timer(val)
+
+
+    def play_music(self, artist, song=None):
+        play=0
+        player = Spotify(self.path.replace("/", "\\")+"\\modules\\spotify")
+        uri = player.get_uri_spotify(artist, song)
+        if not uri==-1: 
+            play=player.play_spotify(uri)
+        elif uri == "":
+            return -1
+        if play: return 1
+        else: return -1
 
 
     def send_request(self, command, model):
@@ -59,9 +74,9 @@ class Command:
         if model == 'model-selector':
             self.response = openai.Completion.create(
                     engine="ada",
-                    prompt="Q: turn off the lights\nA: light-application \nQ: turn on the lights\nA: light-application \nQ: set the lights to sparkle\nA: light-application \nQ: put the lights on sparkle mode\nA: light-application \nQ: how are you doing\nA: conversation-application \nQ: what's the meaning of life\nA: conversation-application \nQ: i hope your day is going well.\nA: conversation-application \nQ: yo\nA: conversation-application \nQ: can you turn the lights off\nA: light-application\nQ: write me a poem\nA: conversation-application \nQ: how do you split and atom in half\nA: conversation-application\nQ: what happens when you mix baking soda and vinegar\nA: conversation-application\nQ: set a timer for 45 seconds\nA: timer-application\nQ: timer 1 minute\nA: timer-application\nQ: turn off the lights\nA: light-application\nQ: what is the meaning of life?\nA: conversation-application\nQ: count down from 50\nA: timer-application\nQ: turn it off\nQ: it's dab time\nA: timer-application\nQ: time to hit a dab\nA: timer-application\nQ: set a dab timer\nA: timer-application\nQ: who are you\nA: conversation-application\nQ: turn off the lights\nA: light-application\nQ: %s" % command,
+                    prompt="Q: turn off the lights\nA: light-application \nQ: turn on the lights\nA: light-application \nQ: set the lights to sparkle\nA: light-application \nQ: put the lights on sparkle mode\nA: light-application \nQ: how are you doing\nA: conversation-application \nQ: what's the meaning of life\nA: conversation-application \nQ: i hope your day is going well.\nA: conversation-application \nQ: yo\nA: conversation-application \nQ: can you turn the lights off\nA: light-application\nQ: write me a poem\nA: conversation-application \nQ: how do you split and atom in half\nA: conversation-application\nQ: what happens when you mix baking soda and vinegar\nA: conversation-application\nQ: set a timer for 45 seconds\nA: timer-application\nQ: timer 1 minute\nA: timer-application\nQ: turn off the lights\nA: light-application\nQ: what is the meaning of life?\nA: conversation-application\nQ: count down from 50\nA: timer-application\nQ: it's dab time\nA: timer-application\nQ: time to hit a dab\nA: timer-application\nQ: set a dab timer\nA: timer-application\nQ: who are you\nA: conversation-application\nQ: turn off the lights\nA: light-application\nQ: play dig my grave by goat\nA: spotify-application\nQ: can we listen to the mars volta\nA: spotify-application\nQ: play crucial by false jasmine on spotify\nA: spotify-application\nQ: play jeff buckley on spotify\nA: spotify-application\nQ: %s" % command,
                     temperature=0,
-                    max_tokens=100,
+                    max_tokens=64,
                     top_p=1,
                     frequency_penalty=0.2,
                     presence_penalty=0,
@@ -71,8 +86,8 @@ class Command:
             self.response = openai.Completion.create(
                 engine="ada",
                 prompt="Q: turn off the lights.\nA: toggle-light `off`\nQ: turn on the lights.\nA: toggle-light `on`\nQ: lights on.\nA: toggle-light `on`\nQ: lights off.\nA: toggle-light `off`\nQ: can you turn the lights on?\nA: toggle-light `on`\nQ: toggle lights\nA: toggle-light `off`\nQ: toggle lights\nA: toggle-light `on`\nQ: set the lights to sparkle\nA: toggle-light `sparkle`\nQ: put the lights on sparkle mode\nA: toggle-light `sparkle`\nQ: sparkle\nA: toggle-light `sparkle`\nQ: turn the lights on.\nA: toggle-light `on`\nQ: set the lights to sparkle\nA: toggle-light `sparkle`\nQ: turn off the lights.\nA: toggle-light `off`\nQ: set the was to sparkle\nA: toggle-light `sparkle`\nQ: %s" % command,
-                temperature=0.05,
-                max_tokens=100,
+                temperature=0,
+                max_tokens=64,
                 top_p=1,
                 frequency_penalty=0.2,
                 presence_penalty=0,
@@ -81,7 +96,7 @@ class Command:
         elif model == 'conversation-application':
             self.response = openai.Completion.create(
                 engine="babbage",
-                prompt="Q: hello.\nA: toggle-conversation `Hello! What's up? \nQ: how are you?\nA: toggle-conversation `I'm doing great!` How are you?\nQ: what is your name?\nA: toggle-conversation `My name is Ditto!`\nQ: what is your name?\nA: toggle-conversation `Ditto.`\nQ: what's the population of Brazil?\nA: toggle-conversation `212.6 million as of 2020.`\nQ: what's the meaning of life?\nA: toggle-conversation `the meaning of life is to love oneself and to spread love to others.`\nQ: can you take the square root of a negative number?\nA: toggle-conversation `The square root of a negative number does not exist among the set of real numbers; however, the imaginary number \"i\" is the square root of negative one.`\nQ: can you write me a poem?\nA: toggle-conversation `Despite the storms, \\nbeauty arrives like \\nit was always going to. \\nDespite the darkness, \\nthe light returns. \\nDespite your loss, \\nyour heart will be \\nfull again. \\nDespite the breaking, \\nyour heart will feel \\nlike it belongs in the \\nland of joy once more. \\nThis is how it will \\nalways be. Keep living.`\nQ: can you tell me who the president of the United States was in 1975?\nA: toggle-conversation `Gerald Ford was the president of the United States in 1975.`\nQ: write me a song\nA: toggle-conversation `I'm not a musician, but I'll give it a shot. \\n \\nFresh day in the simulation \\nSleep all day, it's living \\nFlying at the speed of light, \\nIt's really gonna be alright.`\nQ: write me another song\nA: toggle-conversation `A gift for you \\nA gift for me \\nYou're the one \\nThat lives for me \\nAnd I for you \\nTrue love is thee.`\nQ: write me a cool song\nA: toggle-conversation `It's a rock & roll song. It's about a guy and his friend. \\n\\nThey're in a band. \\nThey want to play at a party. \\nThey're in their living room, playing this song, \\nand it's really loud. \\nAnd the guy is like, \\n\"That's it, that's it! \\nWe're going to go play that party!`\nQ: Who was the 16th president of the united states?\nA: toggle-conversation `Abraham Lincoln was the 16th president of the United States.`\nQ: What is an atom made up of?\nA: toggle-conversation `The atom is made up of protons and neutrons, which have electrons surrounding them.`\nQ: How can I start my day better?\nA: toggle-conversation `Start your day with a good breakfast. \\nStand up and move around in your living room. \\nRelax for a bit. \\nGo for a walk in the park. \\nTry to get some exercise in the afternoon. \\nEat healthier meals.\\nLeave a little more time for the evening. \\Sleep better the day before.`\nQ: Who are you?\nA: toggle-conversation `I'm Ditto! My backend is on GPT-3, a powerful language model made by openai.`\nQ: %s" % command,
+                prompt="Q: hello.\nA: toggle-conversation `Hello! What's up? \nQ: how are you?\nA: toggle-conversation `I'm doing great!` How are you?\nQ: what is your name?\nA: toggle-conversation `My name is Ditto!`\nQ: what is your name?\nA: toggle-conversation `Ditto.`\nQ: what is your purpose?\nA: toggle-conversation `I am here to provide information I was trained on. I will try and be as correct and precise as I can.`\nQ: what's the population of Brazil?\nA: toggle-conversation `212.6 million as of 2020.`\nQ: what's the meaning of life?\nA: toggle-conversation `the meaning of life is to love oneself and to spread love to others.`\nQ: can you take the square root of a negative number?\nA: toggle-conversation `The square root of a negative number does not exist among the set of real numbers; however, the imaginary number \"i\" is the square root of negative one.`\nQ: can you write me a poem?\nA: toggle-conversation `Despite the storms, \\nbeauty arrives like \\nit was always going to. \\nDespite the darkness, \\nthe light returns. \\nDespite your loss, \\nyour heart will be \\nfull again. \\nDespite the breaking, \\nyour heart will feel \\nlike it belongs in the \\nland of joy once more. \\nThis is how it will \\nalways be. Keep living.`\nQ: can you tell me who the president of the United States was in 1975?\nA: toggle-conversation `Gerald Ford was the president of the United States in 1975.`\nQ: write me a song\nA: toggle-conversation `I'm not a musician, but I'll give it a shot. \\n \\nFresh day in the simulation \\nSleep all day, it's living \\nFlying at the speed of light, \\nIt's really gonna be alright.`\nQ: write me another song\nA: toggle-conversation `A gift for you \\nA gift for me \\nYou're the one \\nThat lives for me \\nAnd I for you \\nTrue love is thee.`\nQ: write me a cool song\nA: toggle-conversation `It's a rock & roll song. It's about a guy and his friend. \\n\\nThey're in a band. \\nThey want to play at a party. \\nThey're in their living room, playing this song, \\nand it's really loud. \\nAnd the guy is like, \\n\"That's it, that's it! \\nWe're going to go play that party!`\nQ: Who was the 16th president of the united states?\nA: toggle-conversation `Abraham Lincoln was the 16th president of the United States.`\nQ: What is an atom made up of?\nA: toggle-conversation `The atom is made up of protons and neutrons, which have electrons surrounding them.`\nQ: How can I start my day better?\nA: toggle-conversation `Start your day with a good breakfast. \\nStand up and move around in your living room. \\nRelax for a bit. \\nGo for a walk in the park. \\nTry to get some exercise in the afternoon. \\nEat healthier meals.\\nLeave a little more time for the evening. \\Sleep better the day before.`\nQ: Who are you?\nA: toggle-conversation `I'm Ditto! My backend is on GPT-3, a powerful language model made by openai.`\nQ: how many parameters do you have?\nA: toggle-conversation `I have 175 billion parameters.`\nQ: how do you compare with GPT-2?\nA: toggle-conversation `GPT-3 is a new version of GPT-2. GPT-3 was created to be more robust than GPT-2 in that it is capable of handling more niche topics.`\nQ: how many parameters does GPT-2 have?\nA: toggle-conversation `GPT-2 has 1.5 billion parameters.`\nQ: what is the human population count\nA: toggle-conversation `The human population count is 7,794,798,739 as of 2020.`\nQ: how many notes are there in a scale\nA: toggle-conversation `There are 7 notes in a scale.`\nQ: what is the speed of light?\nA: toggle-conversation `The speed of light is about 300,000 km per second.`\nQ: %s" % command,
                 temperature=0.6,
                 max_tokens=200,
                 top_p=1,
@@ -93,6 +108,17 @@ class Command:
             self.response = openai.Completion.create(
                 engine="ada",
                 prompt="Q: set a timer for 45 seconds\nA: toggle-timer `45 seconds`\nQ: set a timer for 1 minute and 30 seconds\nA: toggle-timer `1 minute 30 seconds`\nQ: timer for 23 seconds\nA: toggle-timer `23 seconds`\nQ: count down from 30\nA: toggle-timer `30 seconds`\nQ: set the timer for 35 seconds\nA: toggle-timer `35 seconds`\nQ: set a timer for one minute\nA: toggle-timer `1 minute`\nQ: it`s dab time\nA: toggle-timer `50 seconds`\nQ: time to hit a dab\nA: toggle-timer `50 seconds`\nQ: set a dab timer\nA:  toggle-timer `50 seconds`\nQ: set a timer for half a minute\nA: toggle-timer `30 seconds`\nQ: set a timer for a minute and a half\nA: toggle-timer `1 minute and 30 seconds`\nQ: set a timer for 2 minutes and a half\nA: toggle-timer `2 minutes and 30 seconds`\nQ: turn the timer on for half a minute\nA: toggle-timer `30 seconds`\nQ: turn the timer on for half a minute and a half\nA: toggle-timer `30 seconds`\nQ: toggle timer for 60 seconds\nA: toggle-timer `60 seconds`\nQ: time to hit a dab\nA: toggle-timer `50 seconds`\nQ: %s" % command,
+                temperature=0,
+                max_tokens=64,
+                top_p=1,
+                frequency_penalty=0.2,
+                presence_penalty=0,
+                stop=["\nQ: "]
+            )
+        elif model == 'spotify-application':
+            self.response = openai.Completion.create(
+                engine="ada",
+                prompt="Q: play crucial by false jasmine.\nA: toggle-spotify `false jasmine, crucial`\nQ: play false jasmine on spotify.\nA: toggle-spotify `false jasmine`\nQ: play false jasmine.\nA: toggle-spotify `false jasmine`\nQ: play vitamin c by can .\nA: toggle-spotify `can, vitamin c`\nQ: can we listen to the mars volta.\nA: toggle-spotify `the mars volta`\nQ: throw on some slum village.\nA: toggle-spotify `slum village`\nQ: play all my love by led zeppelin.\nA: toggle-spotify `led zeppelin, all my love`\nQ: play me some sublime .\nA: toggle-spotify `sublime`\nQ: can we listen to grace by jeff buckley.\nA: toggle-spotify `jeff buckley, grace`\nQ: play roundabout by yes.\nA: toggle-spotify `yes, roundabout`\nQ: play the widow by the mars volta.\nA: toggle-spotify `the mars volta, the widow`\nQ: play deep down by mike patton.\nA: toggle-spotify `mike patton, deep down`\nQ: play djedje by gordon.\nA: toggle-spotify `gordon, djedje`\nQ: play djedje.\nA: toggle-spotify `djedje`\nQ: play dig my grave by goat.\nA: toggle-spotify `goat, dig my grave`\nQ: play the mars volta.\nA: toggle-spotify `the mars volta`\nQ: %s" % command,
                 temperature=0,
                 max_tokens=64,
                 top_p=1,
---
+++ modules/spotify/spotify.py
@@ -0,0 +1,88 @@
+"""
+Spotify application build around the Spotify Web API.
+
+refs:
+1) https://developer.spotify.com/documentation/general/guides/authorization/
+2) https://github.com/plamere/spotipy
+"""
+
+import os
+import json
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+
+
+class Spotify():
+    
+    def __init__(self, path):
+        
+        self.path = path 
+        # os.system(self.path.replace("/", "\\"))
+
+        # first time set up (automated as much as possible)
+        l = []
+        for x in os.listdir(path):
+            l.append(x)
+
+        if not 'resources' in l:
+            os.system('mkdir resources')
+
+        l = []
+        for x in os.listdir(path + '/resources'):
+            l.append(x)
+        if 'spotify' not in x:
+            print('no spotify json found...')
+            s = json.dumps({"client-id": "ID", "client-secret": "ID"})
+            with open('resources/spotify.json', 'w') as f:
+                f.write(s)
+            print('created spotify.json in resources/')
+            print('please fill in client app IDs')
+            exit()
+        
+        with open(path +'/resources/spotify.json') as f:
+            s = ""
+            for x in f.readlines():
+                s += x
+        self.user_values = json.loads(s)
+    
+    def play_spotify(self, uri):
+        scope = "user-read-playback-state,user-modify-playback-state"
+        sp = spotipy.Spotify(
+            client_credentials_manager = SpotifyOAuth(
+                client_id = self.user_values["client-id"],
+                client_secret = self.user_values["client-secret"],
+                scope=scope,
+                redirect_uri='http://127.0.0.1:8123/'
+            )                
+        )
+        try:
+            sp.start_playback(uris=[uri], device_id=0)
+            return 1
+        except:
+            print("invalid uri")
+            return -1
+
+    def get_uri_spotify(self, artist, song=None):
+
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyClientCredentials(
+                client_id = self.user_values["client-id"],
+                client_secret = self.user_values["client-secret"],
+            )
+        )
+        try:
+            results = sp.search(q=artist, limit=30) # change limit for more results
+            for idx, track in enumerate(results['tracks']['items']): 
+                if song == None: return track['uri']
+                if song.lower() in track['name'].lower():
+                    return track['uri']
+        except:
+            print('invalid search')
+            return -1
+    
+
+if __name__ == "__main__":
+    spotify_app = Spotify(os.getcwd())
+    uri = spotify_app.get_uri_spotify("False Jasmine", "gravity")
+    spotify_app.play_spotify(uri)
