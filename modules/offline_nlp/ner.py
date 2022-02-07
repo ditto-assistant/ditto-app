627b63c482f5203ad358a44b4a3d936acc393af5
Omar Barazanji <ozanji345@gmail.com>
2022-02-06 22:12:15 -0600

    NER model working for spotify commands

--- modules/offline_nlp/ner.py
+++ modules/offline_nlp/ner.py
@@ -1,169 +1,132 @@
+from __future__ import unicode_literals, print_function
 '''
 Named Entity Recognition model.
 
 author: Omar Barazanji
 
-refs:
-https://towardsdatascience.com/named-entity-recognition-ner-using-keras-bidirectional-lstm-28cd3f301f54
-
 dataset:
 https://raw.githubusercontent.com/Skuldur/virtual-assistant-tutorial/master/commands/play_commands.json
+
+refs:
+https://towardsdatascience.com/train-ner-with-custom-training-data-using-spacy-525ce748fab7
 '''
 import pandas as pd
 import nltk
 from nltk import pos_tag
 import json 
+import re
 
+import plac
+import random
+from pathlib import Path
+import spacy
+from spacy.training.example import Example
+
+from tqdm import tqdm
+
+# def create_load_data():
 # load in and organize data
 with open('data/play_commands.json') as f:
     json_data = json.load(f)
 
-sentence_num_arr = []
-word_arr = []
-pos_arr = []
-tag_arr = []
-
+sentences = []
+entities = []
 for ndx,data in enumerate(json_data['training_data']):
+    if ndx == 30: break
     words = data['words']
     labels = data['labels']
-    pos = pos_tag(words)
-    sentence_num = ndx+1
-    for w_ndx,w in enumerate(words):
-        if w_ndx==0: sentence_num_arr.append('Sentence: %d' % sentence_num)
-        else: sentence_num_arr.append('')
-        word_arr.append(w)
-        pos_arr.append(pos[w_ndx][1])
-        tag_arr.append(labels[w_ndx])
-    if sentence_num == 1000:
-        break
 
-df_data = pd.DataFrame()
-df_data['Sentence #'] = sentence_num_arr
-df_data['Word'] = word_arr
-df_data['POS'] = pos_arr
-df_data['Tag'] = tag_arr
+    ent_ndx = []
+    for ndx,x in enumerate(labels):
+        if 'song' in x or 'artist' in x or 'playlist':
+            if 'song' in x:
+                tag = 'SONG'
+                ent_ndx.append([ndx, x])
+            elif 'artist' in x:
+                tag = 'ARTIST'
+                ent_ndx.append([ndx, x])
+            elif 'playlist' in x:
+                tag = 'PLAYLIST'
+                ent_ndx.append([ndx, x])
 
 
-# Extract mappings for neural network
-from itertools import chain
-def get_dict_map(data, token_or_tag):
-    tok2idx = {}
-    idx2tok = {}
+
+    sentence = ''
+    for x in words:
+        sentence += x+" "
+    if '(' in sentence or ')' in sentence or '+' in sentence or '?' in sentence or '[' in sentence:
+        continue
     
-    if token_or_tag == 'token':
-        vocab = list(set(data['Word'].to_list()))
-    else:
-        vocab = list(set(data['Tag'].to_list()))
-    
-    idx2tok = {idx:tok for  idx, tok in enumerate(vocab)}
-    tok2idx = {tok:idx for  idx, tok in enumerate(vocab)}
-    return tok2idx, idx2tok
+    ent = []
+    curr_ndx = 0
+    exiting = False
+    for x in ent_ndx:
+        word = words[x[0]]
+        for match in re.finditer(word, sentence):
+            beg = match.start()
+            end = match.end()
+            if end <= curr_ndx:
+                exiting = True
+            curr_ndx = end
+            break
+        if exiting: break
+        ent.append((beg, end, x[1]))
+    if exiting: continue
+    sentences.append(sentence)
+    entities.append(ent)
 
-token2idx, idx2token = get_dict_map(df_data, 'token')
-tag2idx, idx2tag = get_dict_map(df_data, 'tag')
-
-df_data['Word_idx'] = df_data['Word'].map(token2idx)
-df_data['Tag_idx'] = df_data['Tag'].map(tag2idx)
 
 
-# Transform columns to extract sequential data
-data_fillna = df_data.fillna(method='ffill', axis=0)
-data_group = data_fillna.groupby(
-    ['Sentence #'],
-    as_index=False
-)['Word', 'POS', 'Tag', 'Word_idx', 'Tag_idx'].agg(lambda x: list(x))
+TRAIN_DATA = []
 
-print(data_group.head())
+for sent,ent in zip(sentences,entities):
+    ent_dict = dict()
+    ent_dict['entities'] = ent
+    TRAIN_DATA.append((sent,ent_dict))
 
-# split to train and test
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
+# training time!
+model = None
+output_dir=Path("models/ner")
+n_iter=100
 
-def get_pad_train_test_val(data_group, data):
+if model is not None:
+    nlp = spacy.load(model)  
+    print("Loaded model '%s'" % model)
+else:
+    nlp = spacy.blank('en')  
+    print("Created blank 'en' model")
 
-    #get max token and tag length
-    n_token = len(list(set(data['Word'].to_list())))
-    n_tag = len(list(set(data['Tag'].to_list())))
+#set up the pipeline
 
-    #Pad tokens (X var)    
-    tokens = data_group['Word_idx'].tolist()
-    maxlen = max([len(s) for s in tokens])
-    pad_tokens = pad_sequences(tokens, maxlen=maxlen, dtype='int32', padding='post', value= n_token - 1)
+if 'ner' not in nlp.pipe_names:
+    ner = nlp.add_pipe('ner')
+    # nlp.add_pipe(ner, last=True)
+else:
+    ner = nlp.get_pipe('ner')
 
-    #Pad Tags (y var) and convert it into one hot encoding
-    tags = data_group['Tag_idx'].tolist()
-    pad_tags = pad_sequences(tags, maxlen=maxlen, dtype='int32', padding='post', value= tag2idx["O"])
-    n_tags = len(tag2idx)
-    pad_tags = [to_categorical(i, num_classes=n_tags) for i in pad_tags]
-    
-    #Split train, test and validation set
-    tokens_, test_tokens, tags_, test_tags = train_test_split(pad_tokens, pad_tags, test_size=0.1, train_size=0.9, random_state=2020)
-    train_tokens, val_tokens, train_tags, val_tags = train_test_split(tokens_,tags_,test_size = 0.25,train_size =0.75, random_state=2020)
+for _, annotations in TRAIN_DATA:
+    for ent in annotations.get('entities'):
+        ner.add_label(ent[2])
 
-    print(
-        'train_tokens length:', len(train_tokens),
-        '\ntrain_tokens length:', len(train_tokens),
-        '\ntest_tokens length:', len(test_tokens),
-        '\ntest_tags:', len(test_tags),
-        '\nval_tokens:', len(val_tokens),
-        '\nval_tags:', len(val_tags),
-    )
-    
-    return train_tokens, val_tokens, test_tokens, train_tags, val_tags, test_tags
+other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+with nlp.disable_pipes(*other_pipes):  # only train NER
+    optimizer = nlp.begin_training()
+    for itn in range(n_iter):
+        random.shuffle(TRAIN_DATA)
+        losses = {}
+        for text, annotations in tqdm(TRAIN_DATA):
+            doc = nlp.make_doc(text)
+            example = Example.from_dict(doc, annotations)
+            nlp.update(
+                [example],  
+                drop=0.5,  
+                sgd=optimizer,
+                losses=losses)
+        print(losses)
 
-train_tokens, val_tokens, test_tokens, train_tags, val_tags, test_tags = get_pad_train_test_val(data_group, df_data)
-
-# model architecture 
-import numpy as np
-import tensorflow
-from tensorflow.keras import Sequential, Model, Input
-from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
-from tensorflow.keras.utils import plot_model
-
-from numpy.random import seed
-seed(1)
-tensorflow.random.set_seed(2)
-
-input_dim = len(list(set(df_data['Word'].to_list())))+1
-output_dim = 64
-input_length = max([len(s) for s in data_group['Word_idx'].tolist()])
-n_tags = len(tag2idx)
-print('input_dim: ', input_dim, '\noutput_dim: ', output_dim, '\ninput_length: ', input_length, '\nn_tags: ', n_tags)
-
-def get_bilstm_lstm_model():
-    model = Sequential()
-
-    # Add Embedding layer
-    model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=input_length))
-
-    # Add bidirectional LSTM
-    model.add(Bidirectional(LSTM(units=output_dim, return_sequences=True, dropout=0.2, recurrent_dropout=0.2), merge_mode = 'concat'))
-
-    # Add LSTM
-    model.add(LSTM(units=output_dim, return_sequences=True, dropout=0.5, recurrent_dropout=0.5))
-
-    # Add timeDistributed Layer
-    model.add(TimeDistributed(Dense(n_tags, activation="relu")))
-
-    #Optimiser 
-    # adam = k.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999)
-
-    # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.summary()
-    
-    return model
-
-# fit model
-def train_model(X, y, model):
-    loss = list()
-    for i in range(25):
-        # fit model for one epoch on this sequence
-        hist = model.fit(X, y, batch_size=500, verbose=1, epochs=1, validation_split=0.2)
-        loss.append(hist.history['loss'][0])
-    return loss
-
-model_bilstm_lstm = get_bilstm_lstm_model()
-plot_model(model_bilstm_lstm)
-loss = train_model(train_tokens, np.array(train_tags), model_bilstm_lstm)
+if output_dir is not None:
+    output_dir = Path(output_dir)
+    if not output_dir.exists():
+        output_dir.mkdir()
+    nlp.to_disk(output_dir)
+    print("Saved model to", output_dir)
