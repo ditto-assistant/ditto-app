c81b28266a9d191aec3eff01b1dbcf7cd25e504e
Omar Barazanji <ozanji345@gmail.com>
2022-07-18 00:31:53 -0500

    spotify local playrrr

--- resources/export_justin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'export PYGAME_HIDE_SUPPORT_PROMPT=1'
-echo 'export GOOGLE_APPLICATION_CREDENTIALS="/Users/omar/assistant/resources/gpt3-assistant-333702-1c63cfb7009b.json"'
-echo 'export OPENAI_API_KEY="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"'
-echo 'export SPOTIPY_CLIENT_ID="7a18b23e230247169ff5f691f33d3d76"'
-echo 'export SPOTIPY_CLIENT_SECRET="b6388e0f3c9d4aa2bdea2242260572df"'
-echo 'export SPOTIPY_REDIRECT_URI="http://127.0.0.1:8124"'
