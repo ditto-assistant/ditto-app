0a9c331d7809847b05c7cb347975befc63e092a6
Omar Barazanji <ozanji345@gmail.com>
2022-10-17 00:46:11 -0500

    Working Flask API for writing to Ditto.db sqlite3 database. Ready for ditto-app client.

---
+++ modules/server/start_server.py
@@ -0,0 +1,16 @@
+from server import Server
+from gevent.pywsgi import WSGIServer
+import subprocess
+import socket 
+from flask import g
+
+def start_server():
+    server = Server()
+    subprocess.Popen(['python', 'ditto.py'])
+    http_server = WSGIServer(('localhost', 42032), server.app)
+    print('\n\n[Server started on port 42032]\n\n')
+    http_server.serve_forever()
+
+if __name__ == "__main__":
+    start_server()
+    
