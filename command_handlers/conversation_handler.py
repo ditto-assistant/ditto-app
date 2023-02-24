ba4f94a13d063c741b10a94bacb00b80a2be6b83
Omar Barazanji <ozanji345@gmail.com>
2023-02-24 16:02:36 -0600

    latest

--- command_handlers/conversation_handler.py
+++ command_handlers/conversation_handler.py
@@ -16,15 +16,15 @@ class ConversationHandler:
             command.inject_response(prompt+'.', raw_reply) # add user's prompt and gpt3's response to conversation prompt for context
             reply = ""
 
-            if '\\n' in raw_reply: # render newlines from reply
-                raw_reply = raw_reply.split('\\n')
-                print('\nA: ')
-                for x in raw_reply:
-                    print(x)
-                    reply = reply + " " + x.strip('\\') 
-            else: 
-                print('\nA: '+ raw_reply)
-                reply = raw_reply
+            # if '\\n' in raw_reply: # render newlines from reply
+            #     raw_reply = raw_reply.split('\\n')
+            #     print('\nA: ')
+            #     for x in raw_reply:
+            #         print(x)
+            #         reply = reply + " " + x.strip('\\') 
+            # else: 
+            #     print('\nA: '+ raw_reply)
+            reply = raw_reply
         else:
             reply = self.offline_chat.prompt(prompt)
             print('\nA: '+ reply)
