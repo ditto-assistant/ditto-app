e751bfc1a93296a099a4274989fddc722c464522
Omar Barazanji <ozanji345@gmail.com>
2023-03-08 18:20:13 -0600

    fixed HeyDittoNet mem leak

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import gevent.subprocess as subprocess
+import gevent.subprocess 
 
 def start_server():
         '''
         Boots the Ditto Server for API calls.
         '''
         print('\n[Starting server...]')
-        subprocess.call(['python', 'modules/server/start_server.py'])
-        
+        gevent.subprocess.call(['python', 'modules/server/start_server.py'])        
 
 if __name__ == "__main__":
     start_server()
