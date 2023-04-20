60e3973294ededa10693a401017c46bdefcfc8ca
Omar Barazanji <ozanji345@gmail.com>
2023-04-20 15:59:13 -0500

    py lint vscode

--- ditto.py
+++ ditto.py
@@ -8,7 +8,8 @@ author: Omar Barazanji
 # import pyttsx3
 
 # other imports
-import os 
+import sqlite3
+import os
 import time
 import pygame
 from threading import Timer
@@ -18,7 +19,7 @@ from command_handlers.command import Command
 from modules.google_tts.speak import Speak
 from modules.security_camera.security_cam import SecurityCam
 import json
-import platform 
+import platform
 import numpy as np
 
 # used to send keypress event (keeps display on)
@@ -37,7 +38,6 @@ if platform.system() == 'Linux':
 
 OFFLINE_MODE = False
 
-import sqlite3
 
 class Assistant:
 
@@ -46,23 +46,25 @@ class Assistant:
         self.update_status_db('booting')
         self.load_config()
         self.security_camera = SecurityCam(os.getcwd())
-        self.speech = Speech(offline_mode=offline_mode, mic=self.config['microphone'])
+        self.speech = Speech(offline_mode=offline_mode,
+                             mic=self.config['microphone'])
         self.command = Command(os.getcwd(), offline_mode)
         self.speech_engine = ''
         self.google = Speak()
         # self.speech_engine.setProperty('voice', 'english')
         # self.speech_engine.setProperty('rate', 190)
-        self.speech_volume = self.config['volume'] # percent
+        self.speech_volume = self.config['volume']  # percent
         self.prompt = ""
         self.reply = ""
-        self.activation_mode = True # If true then idle and listening for name
+        self.activation_mode = True  # If true then idle and listening for name
 
-        self.reset_conversation = False # used to skip writing reset commands to DB
+        self.reset_conversation = False  # used to skip writing reset commands to DB
 
-        self.skip_name = False # used to skip name (conversation app)
+        self.skip_name = False  # used to skip name (conversation app)
 
-        self.command_timer = 0 # used to handle the timeout of interaction with assistant
-        self.comm_timer_mode = False # will go to false after 5 seconds of inactivity (idle)
+        self.command_timer = 0  # used to handle the timeout of interaction with assistant
+        # will go to false after 5 seconds of inactivity (idle)
+        self.comm_timer_mode = False
         self.update_status_db('on')
 
     def load_config(self):
@@ -84,7 +86,7 @@ class Assistant:
         '''
         Resets the command loop to idle.
         '''
-        self.activation_mode = True # go back to idle... 
+        self.activation_mode = True  # go back to idle...
         self.speech.text = ''
         self.prompt = ''
         self.comm_timer_mode = False
@@ -93,8 +95,10 @@ class Assistant:
 
     def play_sound(self, sound='off'):
         print(f'Gui Mode {self.speech.from_gui}')
-        if self.speech.from_gui: return
-        elif self.reset_conversation: return 
+        if self.speech.from_gui:
+            return
+        elif self.reset_conversation:
+            return
         else:
             pygame.mixer.init()
             pygame.mixer.music.load(f"resources/sounds/ditto-{sound}.mp3")
@@ -106,10 +110,11 @@ class Assistant:
         '''
         For applications that require looping, we can skip wake.
         '''
-        self.speech.activation.activate = True # skip wake-up sequence (name is already called)
+        self.speech.activation.activate = True  # skip wake-up sequence (name is already called)
         self.speech.skip_wake = True
-        self.command_timer = 0 # reset command timer 
-        self.comm_timer_mode = False # (pause to not iterrupt assistant speaking)
+        self.command_timer = 0  # reset command timer
+        # (pause to not iterrupt assistant speaking)
+        self.comm_timer_mode = False
         self.comm_timer.cancel()
 
     def prompt_intent(self, prompt):
@@ -120,15 +125,17 @@ class Assistant:
     def conversation_app(self, action=None):
 
         def conversation_flow():
-            self.reply = self.command.conversation_handler.handle_response(self.command, self.prompt)
-            if self.reply == '': self.reply = '...'
+            self.reply = self.command.conversation_handler.handle_response(
+                self.command, self.prompt)
+            if self.reply == '':
+                self.reply = '...'
             self.tts(self.reply)
-            if not self.speech.from_gui: 
+            if not self.speech.from_gui:
                 # self.skip_wake()
                 self.reset_loop()
             else:
                 self.reset_loop()
-                
+
         if action == 'exit':
             if not self.speech.from_gui:
                 self.reply = '[Exiting Conversation Loop]'
@@ -139,14 +146,14 @@ class Assistant:
         else:
             conversation_flow()
 
-    def send_command(self): # application logic
+    def send_command(self):  # application logic
 
         # grab user's prompt from speech module
         self.prompt = self.speech.text
-        
-        # log the user's prompt 
+
+        # log the user's prompt
         print('\n\nwriting prompt to db...')
-        self.write_prompt_to_db() 
+        self.write_prompt_to_db()
 
         if 'GestureNet' in self.prompt:
             cat = 'gesture'
@@ -158,7 +165,7 @@ class Assistant:
             sub_cat = 'none'
             action = 'none'
         else:
-            # get intent from offline npl module 
+            # get intent from offline npl module
             self.offline_response = json.loads(self.prompt_intent(self.prompt))
             cat = self.offline_response['category']
             sub_cat = self.offline_response['sub_category']
@@ -167,17 +174,20 @@ class Assistant:
         print('\n\n')
         print(cat, sub_cat, action)
 
-        # send prompt to application / category 
-        if  cat == 'lights':
-            self.reply = self.command.light_handler.handle_response(self.prompt)
+        # send prompt to application / category
+        if cat == 'lights':
+            self.reply = self.command.light_handler.handle_response(
+                self.prompt)
             self.tts(self.reply)
             self.reset_loop()
 
         elif cat == 'spotify':
             try:
-                self.reply = self.command.spotify_handler.handle_response(self.command, self.prompt)
+                self.reply = self.command.spotify_handler.handle_response(
+                    self.command, self.prompt)
             except BaseException as e:
-                self.reply = self.command.conversation_handler.handle_response(self.command, self.prompt)
+                self.reply = self.command.conversation_handler.handle_response(
+                    self.command, self.prompt)
             self.tts(self.reply)
             self.reset_loop()
 
@@ -190,30 +200,33 @@ class Assistant:
             except BaseException as e:
                 print(e)
                 self.conversation_app()
-            
+
         elif cat == 'timer':
             try:
-                self.reply = self.command.timer_handler.handle_response(self.command, self.prompt)
+                self.reply = self.command.timer_handler.handle_response(
+                    self.command, self.prompt)
                 self.tts(self.reply)
                 self.reset_loop()
             except BaseException as e:
                 print(e)
                 self.conversation_app()
-            
+
         elif cat == 'gesture':
             if gesture == 'like':
                 # self.reply = f'[GestureNet: {gesture}]'
                 self.prompt = 'turn on the lights'
-                self.reply = self.command.light_handler.handle_response(self.prompt)
+                self.reply = self.command.light_handler.handle_response(
+                    self.prompt)
                 # pyautogui.press('right')
             elif gesture == 'dislike':
                 # self.reply = f'[GestureNet: {gesture}]'
                 # pyautogui.press('left')
                 self.prompt = 'set the lights to sparkle'
-                self.reply = self.command.light_handler.handle_response(self.prompt)
+                self.reply = self.command.light_handler.handle_response(
+                    self.prompt)
             self.tts(self.reply)
             self.reset_loop()
-        
+
         elif cat == 'security':
             if not headless:
                 self.reply = f'[Opening {action} camera.]'
@@ -227,16 +240,18 @@ class Assistant:
 
         elif cat == 'weather':
             try:
-                self.reply = self.command.weather_handler.handle_response(self.command, sub_cat)
+                self.reply = self.command.weather_handler.handle_response(
+                    self.command, sub_cat)
                 self.tts(self.reply)
                 self.reset_loop()
             except BaseException as e:
                 print(e)
                 self.conversation_app()
-            
+
         elif cat == 'wolfram':
             try:
-                self.reply = self.command.wolfram_handler.handle_response(self.command, sub_cat, self.prompt)
+                self.reply = self.command.wolfram_handler.handle_response(
+                    self.command, sub_cat, self.prompt)
                 print(f'\nWolfram Reply len: `{len(self.reply)}`\n')
                 if len(self.reply) > 0 and len(self.reply) < 99:
                     self.tts(self.reply)
@@ -247,7 +262,7 @@ class Assistant:
                 print(e)
                 self.conversation_app()
 
-        elif cat == 'conv': # send to conversation handler
+        elif cat == 'conv':  # send to conversation handler
 
             self.conversation_app(action)
 
@@ -256,22 +271,24 @@ class Assistant:
             self.command.reset_conversation()
             self.reset_loop()
 
-        self.write_response_to_db() # log self.reply
+        self.write_response_to_db()  # log self.reply
 
     def write_response_to_db(self):
-        if not self.reply: self.reply = '[empty]'
+        if not self.reply:
+            self.reply = '[empty]'
         if self.reset_conversation:
-            self.reset_conversation = False # set back to False
+            self.reset_conversation = False  # set back to False
             self.reply = ''
             return
         SQL = sqlite3.connect("ditto.db")
         cur = SQL.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS responses(response VARCHAR)")
         SQL.commit()
-        cur.execute("INSERT INTO responses VALUES('%s')" % self.reply.replace("'", "''"))
+        cur.execute("INSERT INTO responses VALUES('%s')" %
+                    self.reply.replace("'", "''"))
         SQL.commit()
         SQL.close()
-        self.reply = '' # reset for next loop
+        self.reply = ''  # reset for next loop
 
     def write_prompt_to_db(self):
         if self.reset_conversation:
@@ -281,7 +298,8 @@ class Assistant:
         cur = SQL.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS prompts(prompt VARCHAR)")
         SQL.commit()
-        cur.execute("INSERT INTO prompts VALUES('%s')" % self.prompt.replace("'", "''"))
+        cur.execute("INSERT INTO prompts VALUES('%s')" %
+                    self.prompt.replace("'", "''"))
         SQL.commit()
         SQL.close()
 
@@ -300,12 +318,14 @@ class Assistant:
 
     def activation_sequence(self):
         self.activation_mode = True
-        self.speech.record_audio(activation_mode=self.activation_mode) # record audio and listen for name
-        if self.speech.activation.activate: # name has been spoken
+        # record audio and listen for name
+        self.speech.record_audio(activation_mode=self.activation_mode)
+        if self.speech.activation.activate:  # name has been spoken
 
-            if self.speech.reset_conversation: self.reset_conversation = True
+            if self.speech.reset_conversation:
+                self.reset_conversation = True
             self.play_sound('on')
-            self.speaker_timer = 0 # reset speaker + mic timer
+            self.speaker_timer = 0  # reset speaker + mic timer
 
             self.speech.activation.activate = False
             self.speech.text = ""
@@ -313,19 +333,23 @@ class Assistant:
 
             self.activation_mode = False
 
-            self.comm_timer_mode = True # turn on timer
-            self.command_timer = 0 # (can be used per application for detecting user idle to cancel)
-            self.command_timeout_handler(30) # executes every n 'seconds' (used to handle back to idle)
+            self.comm_timer_mode = True  # turn on timer
+            # (can be used per application for detecting user idle to cancel)
+            self.command_timer = 0
+            # executes every n 'seconds' (used to handle back to idle)
+            self.command_timeout_handler(30)
 
-            self.speech.record_audio(activation_mode=self.activation_mode) # record audio and listen for command                
+            # record audio and listen for command
+            self.speech.record_audio(activation_mode=self.activation_mode)
 
-            if self.comm_timer_mode: # command has been spoken (app on enter section)
+            # command has been spoken (app on enter section)
+            if self.comm_timer_mode:
                 self.comm_timer.cancel()
                 print("Q: %s\n" % self.speech.activation.text)
                 self.speech.activation.activate = False
                 self.speech.activation.text = ""
 
-                ## enter application handler ## (main loop)
+                # enter application handler ## (main loop)
                 # while not self.activation_mode:
                 try:
                     self.send_command()
@@ -336,35 +360,35 @@ class Assistant:
                     self.reply = '...'
                     self.tts(self.reply)
                     self.reset_loop()
-                
+
             else:
-                self.speech.activation.activate = False # back to idle ...
+                self.speech.activation.activate = False  # back to idle ...
                 self.speech.activation.text = ""
                 # print('Canceling...')
 
-
     def command_timeout_handler(self, timeout):
-        self.comm_timer = Timer(timeout, self.command_timeout_handler, [timeout])
+        self.comm_timer = Timer(
+            timeout, self.command_timeout_handler, [timeout])
         self.comm_timer.start()
-        if self.comm_timer_mode: 
+        if self.comm_timer_mode:
             self.command_timer += 1
             # print("command timer: %d" % self.command_timer)
         if self.command_timer == timeout:
             self.command_timer = 0
-            print('[command timer reset]\n') 
+            print('[command timer reset]\n')
             self.play_sound('off')
-             # go back to idle ...
+            # go back to idle ...
             self.comm_timer.cancel()
-            self.comm_timer_mode = False # turn off timer
+            self.comm_timer_mode = False  # turn off timer
             self.reset_loop()
-            self.speech.comm_timer_mode = False # send to speech submodule for handling 
- 
+            self.speech.comm_timer_mode = False  # send to speech submodule for handling
 
     def tts(self, reply):
-        if not self.speech.from_gui: # only read reply aloud if command / prompt was spoken by user
+        if not self.speech.from_gui:  # only read reply aloud if command / prompt was spoken by user
             try:
                 if UNIX:
-                    os.system('amixer -q set Master ' + str(self.speech_volume)+'%')
+                    os.system('amixer -q set Master ' +
+                              str(self.speech_volume)+'%')
                 # os.system('pico2wave -w reply.wav "%s" && aplay -q reply.wav' % prompt.strip("[]"))
                 if not self.speech.offline_mode:
                     self.google.gtts(reply)
@@ -373,7 +397,7 @@ class Assistant:
                     self.speech_engine.runAndWait()
             except BaseException as e:
                 print(e)
-    
+
 
 if __name__ == "__main__":
 
