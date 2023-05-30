7ae2b4400726f1273de553e62de4ff8981c6cc84
Omar Barazanji <ozanji345@gmail.com>
2023-05-30 00:42:19 -0500

    latest

--- reinforce_loop.py
+++
@@ -1,8 +0,0 @@
-from modules.ditto_activation.main import HeyDittoNet
-heyditto = HeyDittoNet(
-    model_type='CNN-LSTM',
-    path='modules/ditto_activation/',
-    tflite=True
-)
-while True:
-    heyditto.listen_for_name(reinforce=True)
