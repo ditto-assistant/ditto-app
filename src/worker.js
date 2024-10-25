880427f765807c42009f158714fbfb4ddd2f74dc
Peyton-Spencer <pas1234567@gmail.com>
2024-10-24 22:39:47 -0400

    stop manual unregister when we launch the new version

--- src/worker.js
+++ src/worker.js
@@ -4,26 +4,22 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('activate', function (event) {
   event.waitUntil(
-    self.registration.unregister()
-      .then(function () {
-        return navigator.serviceWorker.getRegistrations();
-      })
-      .then(function (registrations) {
-        return Promise.all(registrations.map(function (registration) {
-          if (registration.active && registration.active.scriptURL.endsWith('service-worker.js')) {
-            return registration.unregister();
-          }
-        }));
-      })
-      .then(function () {
-        return self.clients.matchAll();
-      })
-      .then(function (clients) {
-        clients.forEach(client => {
-          if (client.url && 'navigate' in client) {
-            client.navigate(client.url);
-          }
-        });
-      })
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function () {
+      return self.clients.matchAll();
+    }).then(function (clients) {
+      return Promise.all(
+        clients.map(function (client) {
+          return client.navigate(client.url);
+        })
+      );
+    }).then(function () {
+      return self.registration.unregister();
+    })
   );
 });
