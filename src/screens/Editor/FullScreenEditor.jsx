665a3d65a5304d20d2bc4c8b2c82079fc5634e34
Peyton-Spencer <Peyton-Spencer@users.noreply.github.com>
2025-02-25 09:25:13 +0000

    Prettified Code!

--- src/screens/Editor/FullScreenEditor.jsx
+++ src/screens/Editor/FullScreenEditor.jsx
@@ -904,16 +904,16 @@ const FullScreenEditor = ({ script, onClose, onSave }) => {
             width: isMobile
               ? "100%"
               : isMaximized === "editor"
-              ? "100%"
-              : isMaximized === "preview"
-              ? "0%"
-              : `${splitPosition}%`,
+                ? "100%"
+                : isMaximized === "preview"
+                  ? "0%"
+                  : `${splitPosition}%`,
             height: isMobile
               ? isMaximized === "editor"
                 ? "100%"
                 : isMaximized === "preview"
-                ? "0%"
-                : `${splitPosition}%`
+                  ? "0%"
+                  : `${splitPosition}%`
               : "100%",
           }}
           transition={{ type: "spring", bounce: 0, duration: 0.4 }}
@@ -1100,16 +1100,16 @@ const FullScreenEditor = ({ script, onClose, onSave }) => {
             width: isMobile
               ? "100%"
               : isMaximized === "preview"
-              ? "100%"
-              : isMaximized === "editor"
-              ? "0%"
-              : `${100 - splitPosition}%`,
+                ? "100%"
+                : isMaximized === "editor"
+                  ? "0%"
+                  : `${100 - splitPosition}%`,
             height: isMobile
               ? isMaximized === "preview"
                 ? "100%"
                 : isMaximized === "editor"
-                ? "0%"
-                : `${100 - splitPosition}%`
+                  ? "0%"
+                  : `${100 - splitPosition}%`
               : "100%",
           }}
           transition={{ type: "spring", bounce: 0, duration: 0.4 }}
