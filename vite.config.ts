77d77f442aacfd1bc1eba8cca7cc28610af371cd
Peyton-Spencer <pas1234567@gmail.com>
2025-05-22 00:56:49 -0400

    remove legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import legacy from "@vitejs/plugin-legacy"
 import { VitePWA } from "vite-plugin-pwa"
 import tailwindcss from "@tailwindcss/vite"
 import { resolve } from "path"
