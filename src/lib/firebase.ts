6ee67edcc9b4f22cfcc90c32e82943ca749f98c1
Omar Barazanji <ozanji345@gmail.com>
2025-07-06 18:21:54 -0500

    cleaned up

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,9 @@
 import { firebaseConfig } from "@/firebaseConfig"
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const db = getFirestore(app)
