d3edf2c87c792e7387f160983a21a64aa5f9a57f
Omar Barazanji <ozanji345@gmail.com>
2025-07-25 01:48:49 -0400

    release notes

--- src/components/WhatsNew/WhatsNew.tsx
+++ src/components/WhatsNew/WhatsNew.tsx
@@ -36,6 +36,7 @@ import V0_15_7 from "./versions/V0_15_7"
 import V0_16_0 from "./versions/V0_16_0"
 import V0_16_1 from "./versions/V0_16_1"
 import V0_16_2 from "./versions/V0_16_2"
+import V0_17_0 from "./versions/V0_17_0"
 // Add imports for future versions here
 
 // Map versions to their components
@@ -74,6 +75,7 @@ const versionComponents: Record<string, React.ComponentType> = {
   "0.16.0": V0_16_0,
   "0.16.1": V0_16_1,
   "0.16.2": V0_16_2,
+  "0.17.0": V0_17_0,
   // Add future versions here
 }
 
